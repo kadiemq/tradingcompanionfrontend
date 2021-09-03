f954099d3b03da3f69e0f1eaacad0695370d9d2d
Kadiem Alqazzaz <kadiemq@gmail.com>
2021-09-03 20:00:42 +0400

    Started working on new homepage

--- src/App.js
+++ src/App.js
@@ -1,59 +1,22 @@
 import './App.css';
-import {useEffect} from 'react';
-// const fs = require('fs');
-import { createChart, CrosshairMode } from 'lightweight-charts';
+import React, {useEffect} from 'react';
+
+import CryptoBar from "./component_crypto_bar/CryptoBar";
 
 function App() {
 
+
+
+
+
   useEffect(() => {
-
-    const div = document.getElementsByClassName('chart')[0]
-    const validated = false;
-
-      fetch('https://klinesdata.s3.amazonaws.com/BTCUSDT_1m.json')
-          .then((r) => r.json())
-          .then((r) => {
-              console.log(r)
-
-              const chart = createChart(div, { width: 600, height: 300, crosshair: { mode: CrosshairMode.Normal},
-                  timeScale: {
-                      borderColor: 'rgba(197, 203, 206, 0.8)',
-                      timeVisible: true,
-                      secondsVisible: false,
-                  },});
-              const candleSeries = chart.addCandlestickSeries();
-              candleSeries.setData(r)
-
-              const last_timestamp = r[r.length - 1].time
-
-
-
-              const wss = new WebSocket('wss://stream.binance.com:9443/ws/btcusdt@kline_1m')
-
-              wss.onmessage = (e) => {
-                const msg = JSON.parse(e.data)
-                const candle = msg.k;
-
-                // if (candle.t / 1000 - last_timestamp > 60) {
-                //   console.log('a timestamp was skipped!!!')
-                // }
-
-                candleSeries.update({
-                  time: candle.t / 1000,
-                  open: candle.o,
-                  high: candle.h,
-                  low: candle.l,
-                  close: candle.c
-                })
-              }
-          })
   })
 
   return (
     <div >
-      <div className={'chart'}>
 
-      </div>
+      <CryptoBar />
+      {/*<Chart />*/}
     </div>
   );
 }
